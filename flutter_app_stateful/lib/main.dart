9c0b854c04452dcfb51492894cf86393200dcaa8
Lounes Allouti <sahit.sahitou@gmail.com>
2025-11-24 21:20:29 +0100

    Ajout des trois boutons de contrôle (-, Réinitialiser, +)

--- flutter_app_stateful/lib/main.dart
+++ flutter_app_stateful/lib/main.dart
@@ -22,15 +22,38 @@ class _MyWidgetState extends State<MyWidget> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("StatefulWidget"),
-              Text("Le compteur est a : $compteur"),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    compteur++;
-                  });
-                },
-                child: Text("Incrémenter"),
+              Text(" StatefulWidget"),
+              Text("La valeur du compteur est : $compteur"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        compteur--;
+                      });
+                    },
+                    child: Text("-"),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        compteur = 0;
+                      });
+                    },
+                    child: Text("Réinitialiser"),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        compteur++;
+                      });
+                    },
+                    child: Text("+"),
+                  ),
+                ],
               ),
             ],
           ),
