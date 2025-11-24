7ea313149078f2f1e73e11e823a35bd4b3d1c290
Lounes Allouti <sahit.sahitou@gmail.com>
2025-11-24 21:04:42 +0100

    Création du StatefulWidget avec une interface de base

---
+++ flutter_app_stateful/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Stateful Widget"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
