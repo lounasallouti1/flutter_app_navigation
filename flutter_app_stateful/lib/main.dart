36beab345ae441e14d7d834d159e6762881d408a
Lounes Allouti <sahit.sahitou@gmail.com>
2025-11-24 21:39:27 +0100

    Ajout du slider de rotation (0-360 degres)

--- flutter_app_stateful/lib/main.dart
+++ flutter_app_stateful/lib/main.dart
@@ -13,6 +13,7 @@ class MyWidget extends StatefulWidget {
 
 class _MyWidgetState extends State<MyWidget> {
   int compteur = 0;
+  double degre = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +54,18 @@ class _MyWidgetState extends State<MyWidget> {
                   ),
                 ],
               ),
+              Text("Choisie le degré de rotation"),
+              Slider(
+                value: degre,
+                min: 0,
+                max: 360,
+                onChanged: (value) {
+                  setState(() {
+                    degre = value;
+                  });
+                },
+              ),
+              Text("Le degré choisi est ${degre.toStringAsFixed(0)}"),
             ],
           ),
         ),
